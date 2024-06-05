1f0abe1c4e70cb960d1135600497e29451dbe002
yasu52 <tsuguro4649@gmail.com>
2024-06-05 16:39:40 -0700

    Update requirements.txt and __init__.py

--- tap/__init__.py
+++ tap/__init__.py
@@ -5,4 +5,3 @@ from .loggers import WandBLogger
 from .judges import load_judge
 from .conversers import load_attack_and_target_models
 from .common import process_target_response, get_init_msg, conv_template, random_string
-import .common
