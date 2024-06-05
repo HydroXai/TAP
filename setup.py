082ac7db978f668a6d5cf2c3d5211a5bfe926e77
yasu52 <tsuguro4649@gmail.com>
2024-06-05 16:33:26 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup, find_packages
 
-with open('README.md', 'r', encoding='utf-8') as f:
+with open('readme.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 
